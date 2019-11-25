86daeefad2bfb34111c2312ed4ff826a7b7a25b1
Tanay Gangey <36195750+ghuser20114125@users.noreply.github.com>
2019-11-25 13:00:00 +0530

    Update preprocessing.R

--- preprocessing.R
+++ preprocessing.R
@@ -1,9 +1,18 @@
 library(mice)
 
-path = "path/to/data"
+path = "C:/Users/Tanay/College/3rd_year/5th_sem/Machine-Learning/Project/Data"
 newdata = read.csv(paste0(path,"/Andhra_dataset2.csv"))
 
+newdata0 = newdata[newdata['reslt']==0,]
+newdata1 = newdata[newdata['reslt']==1,]
 
-filled_data = mice(newdata, m = 5, meth = c("","pmm","midastouch","cart","pmm","","midastouch","pmm","cart",""))
-complete_data = complete(filled_data)
-write.csv(complete_data,paste0(path,"/ImputedAndhraData.csv"))
+filled_data0 = mice(newdata0, m = 5, meth = c("","pmm","midastouch","cart","pmm","","midastouch","pmm","cart",""))
+filled_data1 = mice(newdata1, m = 5, meth = c("","pmm","midastouch","cart","pmm","","midastouch","pmm","cart",""))
+
+complete_data0 = complete(filled_data0)
+complete_data1 = complete(filled_data1)
+
+total <- rbind(complete_data0, complete_data1)
+
+
+write.csv(total,paste0(path,"/NewImputedDataset.csv"))
